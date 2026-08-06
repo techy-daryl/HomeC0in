6194d698426ecb30bec8e5e58a40681c7e69a19f
techy-daryl <darylrozarioa@gmail.com>
2026-08-05 21:08:34 -0400

    Stop tracking firebase-config.js

---
+++ firebase-config example.js
@@ -0,0 +1,9 @@
+/*here is the code to connect to fire base just type in the values 
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+}; */
