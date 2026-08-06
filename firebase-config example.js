f48411d6226e31919c84d6b21e790619b0f57b44
techy-daryl <darylrozarioa@gmail.com>
2026-08-05 21:14:50 -0400

    Ignore Firebase config

--- firebase-config example.js
+++ firebase-config example.js
@@ -1,9 +1,13 @@
-/*here is the code to connect to fire base just type in the values 
+// Copy this file to firebase-config.js
+// Then replace the placeholder values with your Firebase config.
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-}; */
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_BUCKET",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+export { firebaseConfig };
